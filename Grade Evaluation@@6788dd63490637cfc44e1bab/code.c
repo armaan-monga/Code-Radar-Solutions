1733d17b28fe420038b8c60bf975f58705346cac
armaan-monga <mearmaan661@gmaIl.com>
2025-02-05 18:31:32 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+    char grade;
+
+    // Prompt the user to enter the grade
+    printf("Enter the grade (A, B, C, D, F): ");
+    scanf(" %c", &grade); // Note the space before %c to consume any leftover newline
+
+    // Use a switch statement to determine the description
+    switch (grade) {
+        case 'A':
+            printf("Excellent\n");
+            break;
+        case 'B':
+            printf("Good\n");
+            break;
+        case 'C':
+            printf("Average\n");
+            break;
+        case 'D':
+            printf("Below Average\n");
+            break;
+        case 'F':
+            printf("Fail\n");
+            break;
+        default:
+            printf("Invalid grade\n");
+            break;
+    }
+
+    return 0; // Indicate successful execution
+}
