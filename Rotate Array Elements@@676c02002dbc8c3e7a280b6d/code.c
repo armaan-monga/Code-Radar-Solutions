b3a14861db6633640ebb43edec4fa341172a875c
armaan-monga <mearmaan661@gmaIl.com>
2025-03-28 13:08:04 +0530

    Solution to Rotate Array Elements

--- Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
+++ Rotate Array Elements@@676c02002dbc8c3e7a280b6d/code.c
@@ -1,18 +1,46 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int a[1000],n,k,i;
-    scanf("%d",&n);
-    for(i=0;i<n;i++){
-        scanf("%d",&a[i]);
+// Function to reverse a portion of the array
+void reverse(int arr[], int start, int end) {
+    while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
     }
-    scanf("%d",&k);
-    int temp=a[n-1];
-    for(i=n-1;i>=1;i--){
-        a[i]=a[i-1];
-    }a[i]=temp;
-    for(i=0;i<n;i++){
-        printf("%d\n",a[i]);
+}
+
+int main() {
+    int a[1000], n, k, i;
+    
+    // Input size of array
+    scanf("%d", &n);
+    
+    // Input array elements
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
     }
+    
+    // Input number of rotations
+    scanf("%d", &k);
+    
+    // Optimize k to avoid unnecessary rotations
+    k = k % n;
+    
+    // Step 1: Reverse the entire array
+    reverse(a, 0, n - 1);
+    
+    // Step 2: Reverse the first k elements
+    reverse(a, 0, k - 1);
+    
+    // Step 3: Reverse the remaining n-k elements
+    reverse(a, k, n - 1);
+    
+    // Output the rotated array
+    for (i = 0; i < n; i++) {
+        printf("%d\n", a[i]);
+    }
+    
     return 0;
 }
